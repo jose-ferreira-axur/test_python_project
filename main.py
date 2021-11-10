c176818e3ba317b2c76be14a96b065a52b67d2db
Jose Ferreira <jose.ferreira@axur.com>
2021-11-10 15:07:15 -0300

    First commit

---
+++ main.py
@@ -0,0 +1,10 @@
+import requests
+
+
+def main():
+    result = requests.get("https://www.axur.com.br/")
+    print("OK, status={}".format(result.status_code))
+
+
+if __name__ == "__main__":
+    main()
