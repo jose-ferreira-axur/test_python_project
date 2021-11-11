8afa0237a24d858be88cac691636d9c7e2c7f6c1
Jose Ferreira <jose.ferreira@axur.com>
2021-11-11 18:15:09 -0300

    Another commit 5

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ import requests
 
 def main():
     result = requests.get("https://www.axur.com.br/")
-    print("OK4, status={}".format(result.status_code))
+    print("OK5, status={}".format(result.status_code))
 
 
 if __name__ == "__main__":
